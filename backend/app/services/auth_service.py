a460ec4330196fc47522fe07112bec4bf57d409e
martin-cao <martincao119@icloud.com>
2024-12-16 16:35:25 +0800

    refactor(backend/service): Adjust structure of auth_service.py - Move CRUD codes into models itselves - Improve structure of auth_service.py

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,45 +1,55 @@
-from backend.app.models.user import User
+from backend.app.models.user import User, check_password
 from backend.app import db
 
-def register_user(user_data):
+def register_user(user: User):
     """
     注册用户
-    :param user_data: 用户输入的数据
+    :param user
     :return: 操作结果
     """
-    # 示例实现：检查用户名是否已存在，保存用户数据到数据库
-    # 这里用简单的字典模拟数据库
-    username=user_data.get('username')
-    password=user_data.get('password')
+    # Check whether username exists, then save the data into the database
+    username = user.username
+    password = user.password_sha256
+
     if not username or not password:
-        return "用户名和密码不能为空"
+        return False, "用户名和密码不能为空"
 
     if User.query.filter_by(username=username).first():
-        return "用户已存在"
+        return False, "用户已存在"
 
-def login_user(username, password):
+def login_user(user: User):
     """
     用户登录
-    :param username: 用户名
-    :param password: 密码
+    :param user
     :return: 登录结果
     """
+    username = user.username
+    password = user.password_sha256
+
     user=User.query.filter_by(username=username).first()
     if not user:
-        return("用户名不存在")
+        return False, "用户名不存在"
     if not user.check_password(password):
-        return "密码错误"
+        return False, "密码错误"
     return True
+
 def logout_user():
     """
     用户登出
+    This function is currently not used, since there is no log in data stored in the backend database.
     :return: 登出结果
     """
     return True
-def edit_user(user_data):
-    """# 编辑用户信息逻辑"""
-    username = user_data.get('username')
-    new_password = user_data.get('password')
+
+def edit_user(user, new_password):
+    """
+    编辑用户信息
+    :param user:
+    :return:
+    """
+
+    username = user.username
+    password = user.password_sha256
 
     if not username or not new_password:
         return "用户名和密码不能为空"
@@ -47,7 +57,7 @@ def edit_user(user_data):
     # 查找用户
     user = User.query.filter_by(username=username).first()
     if not user:
-        return "用户不存在"
+        return False, "用户不存在"
 
     # 更新密码
     user.set_password(new_password)
