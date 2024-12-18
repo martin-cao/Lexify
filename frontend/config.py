870c7c2c5003bd4285811385773ab6102b28afd5
martin-cao <martincao119@icloud.com>
2024-12-18 16:56:18 +0800

    feat(frontend/service): Build service for authentication
    - Add auth_service.py
    - Add SERVER_URL in config.py

--- frontend/config.py
+++ frontend/config.py
@@ -5,3 +5,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(basedir, "database", "database_fe.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SECRET_KEY = 'your_secret_key'
+    SERVER_URL = "http://127.0.0.1:5000"
